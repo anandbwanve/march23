43939d7ad44d7e3b29cdbdbec264e89be7a1069c
Anand Wanve <anandbwanve@gmail.com>
2023-05-23 11:50:41 +0530

    sum.java java for main branch

---
+++ sum.java
@@ -0,0 +1,9 @@
+class sum{
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 15;
+        int c = a+b;
+        System.out.println("hello anand" + c);
+        System.out.println("hmm");
+    }
+}
